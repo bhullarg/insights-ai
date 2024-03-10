f1a0c9c483e38a038dac0aa4b70adf5d0b7a1238
Guri Bhullar <gbsbhullar@gmail.com>
2024-03-09 17:09:46 -0800

    commenting code

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ def main():
     response = completion(model="gpt-3.5-turbo", messages=messages)
     print(response)
 
-    query = {
-        'question': 'What are my competitors doing that I should pay attention to?',
-        'context': 
-        """
-        You are a senior data analyst and your job is to discover unique insights about competitors that can inform how my business should adapt its business strategies to stand out and become more competitive. Do not give me obvious insights, focus on uncovering hidden and implied strategies. Outline what the competitors strategies like are and then provide a recommendation for my company. Ask me what my company does and verify your assumptions with me before conducting your analysis.
-        """
-    }
+    # query = {
+    #     'question': 'What are my competitors doing that I should pay attention to?',
+    #     'context': 
+    #     """
+    #     You are a senior data analyst and your job is to discover unique insights about competitors that can inform how my business should adapt its business strategies to stand out and become more competitive. Do not give me obvious insights, focus on uncovering hidden and implied strategies. Outline what the competitors strategies like are and then provide a recommendation for my company. Ask me what my company does and verify your assumptions with me before conducting your analysis.
+    #     """
+    # }
     
-    ques_to_text = pipeline("question-answering", model="deepset/tinyroberta-squad2")
-    answer = ques_to_text(query)['answer']
-    print('##############')
-    print(answer)
-    print('##############')
+    # ques_to_text = pipeline("question-answering", model="deepset/tinyroberta-squad2")
+    # answer = ques_to_text(query)['answer']
+    # print('##############')
+    # print(answer)
+    # print('##############')
 
 main()
