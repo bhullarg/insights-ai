3182a99ac0668b9b6321f42c7775c50d789834e0
Guri Bhullar <gbsbhullar@gmail.com>
2024-03-09 17:05:34 -0800

    gpt3 demo

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
 from transformers import pipeline
+from litellm import completion
+import os
+
+os.environ["OPENAI_API_KEY"] = "" 
 
 def main():
+    messages = [{ "content": "Hello, how are you?","role": "user"}]
+    response = completion(model="gpt-3.5-turbo", messages=messages)
+    print(response)
+
     query = {
-        'question': 'What are skills requried?',
+        'question': 'What are my competitors doing that I should pay attention to?',
         'context': 
         """
-        STSI, the definitive nationwide technical placement firm, is at your service for the technical staffing needs of your company. As the business climate dictates the hiring of skilled individuals - STSI is there with quality staffing solutions who possess the precise skills, experience and educational requirements to fulfill your needs. STSI completes the void in your manpower with qualified individuals while allowing you to focus on the core undertaking of your business - prospering!
+        You are a senior data analyst and your job is to discover unique insights about competitors that can inform how my business should adapt its business strategies to stand out and become more competitive. Do not give me obvious insights, focus on uncovering hidden and implied strategies. Outline what the competitors strategies like are and then provide a recommendation for my company. Ask me what my company does and verify your assumptions with me before conducting your analysis.
         """
     }
     
     ques_to_text = pipeline("question-answering", model="deepset/tinyroberta-squad2")
     answer = ques_to_text(query)['answer']
+    print('##############')
     print(answer)
+    print('##############')
 
 main()
